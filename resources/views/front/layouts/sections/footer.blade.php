7ed2a503af13d499603376af93f1bf62d8e370f5
Front End Ten <programmer@dogandrooster>
2020-01-23 10:35:49 +0800

    Homepage Done

--- resources/views/front/layouts/sections/footer.blade.php
+++ resources/views/front/layouts/sections/footer.blade.php
@@ -1,52 +1,115 @@
 <a href="#" id="to-top"><i class="fa fa-angle-double-up"></i></a>
-<footer>
-    <div class="footer__menu container">
-        <div class="footer__wrapper--row row">
-            <div class="col-lg-4 footer__item">
-                <article>
-                    <h4>
-                        General
-                    </h4>
-                    <ul>
-                        <li><a href="#"> Home </a></li>
-                        <li><a href="#"> Home </a></li>
-                        <li><a href="#"> Home </a></li>
-                        <li><a href="#"> Home </a></li>
-                    </ul>
-                </article>
+<footer class="footer__wrapper image-background">
+    <img src="{{ asset('public/images/footer/footer-bg.png') }}" alt="BG Image">
+    <div class="container-max">
+        <div class="row">
+            <div class="col-md-3 footer__item">
+                <div class="footer-logo">
+                    <a href="{{ url('/') }}">
+                        <img src="{{ asset('public/images/footer/footer-logo.png') }}" alt="logo">
+                    </a>
+                </div>
+                <div class="footer-contacts">
+                    <div class="item"><i class="loc"></i> <span>3990 Old Town Avenue C304, San Diego, CA 92110</span></div>
+                    <div class="item"><i class="tel"></i> <a href="tel:619.795.7873 ">619.795.7873 </a></div>
+                    <div class="item"><i class="mail"></i> <a href="mailto:contact@areaa.org">contact@areaa.org</a></div>
+                </div>
             </div>
-            <div class="col-lg-4 footer__item">
-                <article>
-                    <h4>
-                        General
-                    </h4>
-                    <ul>
-                        <li><a href="#"> Home </a></li>
-                        <li><a href="#"> Home </a></li>
-                        <li><a href="#"> Home </a></li>
-                        <li><a href="#"> Home </a></li>
-                    </ul>
-                </article>
-            </div>
-            <div class="col-lg-4 footer__item">
-                <article>
-                    <h4>
-                        General
-                    </h4>
-                    <ul>
-                        <li><a href="#"> Home </a></li>
-                        <li><a href="#"> Home </a></li>
-                        <li><a href="#"> Home </a></li>
-                        <li><a href="#"> Home </a></li>
-                    </ul>
-                </article>
+            <div class="offset-1 col-md-8 footer__item">
+                <div class="row">
+                    <div class="col-md-3 footer__item">
+                        <article>
+                            <h4>About Us</h4>
+                            <ul>
+                                <li><a href="#"> About AREAA </a></li>
+                                <li><a href="#"> Executive Board </a></li>
+                                <li><a href="#"> National Board </a></li>
+                                <li><a href="#"> Our Partners </a></li>
+                                <li><a href="#"> FAQ </a></li>
+                            </ul>
+                        </article>
+                    </div>
+                    <div class="col-md-3 footer__item">
+                        <article>
+                            <h4>Advocacy</h4>
+                            <ul>
+                                <li><a href="#"> AREAA Timeline </a></li>
+                                <li><a href="#"> How to get involved </a></li>
+                                <li><a href="#"> 3 Point Plan </a></li>
+                            </ul>
+                        </article>
+                    </div>
+                    <div class="col-md-6 footer__item">
+                        <article>
+                            <h4>Resources</h4>
+                            <ul>
+                                <li><a href="#"> a | r | e Magazine </a></li>
+                                <li><a href="#"> State of Asia America report </a></li>
+                                <li><a href="#"> Photo gallery </a></li>
+                                <li><a href="#"> Webinars </a></li>
+                                <li><a href="#"> In Language Support </a></li>
+                            </ul>
+                        </article>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-3 footer__item">
+                        <article>
+                            <h4>Membership</h4>
+                            <ul>
+                                <li><a href="#"> Why join </a></li>
+                                <li><a href="#"> Benefits </a></li>
+                                <li><a href="#"> Find a Member </a></li>
+                                <li><a href="#"> Chapter Locations </a></li>
+                                <li><a href="#"> A-List </a></li>
+                            </ul>
+                        </article>
+                    </div>
+                    <div class="col-md-3 footer__item">
+                        <article>
+                            <h4>Events</h4>
+                            <ul>
+                                <li><a href="#"> Leadership Summit</a></li>
+                                <li><a href="#">Global & Luxury Summit</a></li>
+                                <li><a href="#">Regional Retreats</a></li>
+                                <li><a href="#">National Convention</a></li>
+                                <li><a href="#">Chapter Events </a></li>
+                                <li><a href="#">How to get involved </a></li>
+                            </ul>
+                        </article>
+                    </div>
+                    <div class="col-md-6 footer__item">
+                        <div class="join-content">
+                            <div class="title">
+                                <h4>Join AREAA today!</h4>
+                            </div>
+                            <div class="text-center">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                                <div class="btn-wrap">
+                                    <a href="#" class="btn btn--secondary">Join</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    <div class="footer__copyright container">
-        <div class="footer__wrapper--row row">
-            <div class="col-lg-12 footer__item">
-                Copyright
+    <div class="footer__copyright">
+        <div class="container-max">
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="copyright-txt">
+                        &#169; Copyright 2019 AREAA. All Rights Reserved.    | <a href="#">Web Design by Dog and Rooster, Inc.</a>    |     <a href="#">Privacy Policy</a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="smi-wrap">
+                        <a href="#" title="facebook" class="fb"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#" title="twitter" class="tw"><i class="fab fa-twitter"></i></a>
+                        <a href="#" title="instagram" class="ig"><i class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
