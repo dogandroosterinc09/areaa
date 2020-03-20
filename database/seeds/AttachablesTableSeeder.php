75cb0bad8d000aaea8c99123c90f2071e33c05b9
dnr_mark <programmer20@dogandrooster.com>
2020-03-20 11:02:26 +0800

    board member archive integration

--- database/seeds/AttachablesTableSeeder.php
+++ database/seeds/AttachablesTableSeeder.php
@@ -16,6 +16,78 @@ class AttachablesTableSeeder extends Seeder
 
         \DB::table('attachables')->delete();
         
+        \DB::table('attachables')->insert(array (
+            0 => 
+            array (
+                'id' => 1,
+                'attachment_id' => 68,
+                'attachable_id' => 2,
+                'attachable_type' => 'App\\Models\\BoardMember',
+            ),
+            1 => 
+            array (
+                'id' => 2,
+                'attachment_id' => 69,
+                'attachable_id' => 3,
+                'attachable_type' => 'App\\Models\\BoardMember',
+            ),
+            2 => 
+            array (
+                'id' => 3,
+                'attachment_id' => 70,
+                'attachable_id' => 4,
+                'attachable_type' => 'App\\Models\\BoardMember',
+            ),
+            3 => 
+            array (
+                'id' => 4,
+                'attachment_id' => 71,
+                'attachable_id' => 5,
+                'attachable_type' => 'App\\Models\\BoardMember',
+            ),
+            4 => 
+            array (
+                'id' => 5,
+                'attachment_id' => 72,
+                'attachable_id' => 6,
+                'attachable_type' => 'App\\Models\\BoardMember',
+            ),
+            5 => 
+            array (
+                'id' => 6,
+                'attachment_id' => 73,
+                'attachable_id' => 7,
+                'attachable_type' => 'App\\Models\\BoardMember',
+            ),
+            6 => 
+            array (
+                'id' => 7,
+                'attachment_id' => 74,
+                'attachable_id' => 8,
+                'attachable_type' => 'App\\Models\\BoardMember',
+            ),
+            7 => 
+            array (
+                'id' => 8,
+                'attachment_id' => 75,
+                'attachable_id' => 9,
+                'attachable_type' => 'App\\Models\\BoardMember',
+            ),
+            8 => 
+            array (
+                'id' => 9,
+                'attachment_id' => 76,
+                'attachable_id' => 10,
+                'attachable_type' => 'App\\Models\\BoardMember',
+            ),
+            9 => 
+            array (
+                'id' => 10,
+                'attachment_id' => 77,
+                'attachable_id' => 11,
+                'attachable_type' => 'App\\Models\\BoardMember',
+            ),
+        ));
         
         
     }
