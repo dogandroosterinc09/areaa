d0c99065242391967830662814a706eb965d8c64
dnr_william <frontend@dogandrooster.com>
2020-03-20 11:14:11 +0800

    metro

--- resources/views/front/layouts/sections/chapter-atlantametro/header_chapter_atlantametro.blade.php
+++ resources/views/front/layouts/sections/chapter-atlantametro/header_chapter_atlantametro.blade.php
@@ -44,7 +44,7 @@
                         </li>
                         
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('atlantametro-login') }}">Events</a>
+                            <a class="nav-link" href="{{ url('atlantametro-events') }}">Events</a>
                         </li>
                         <li class="nav-item dropdown">
                             <a class="nav-link" href="{{ url('atlantametro-leadership') }}">Leadership</a>
