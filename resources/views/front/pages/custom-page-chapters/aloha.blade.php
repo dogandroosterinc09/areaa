7683f8ceca81d00d017f121416cd083901d42151
dnr_william <frontend@dogandrooster.com>
2020-03-24 10:37:12 +0800

    adjsut static link for events

--- resources/views/front/pages/custom-page-chapters/aloha.blade.php
+++ resources/views/front/pages/custom-page-chapters/aloha.blade.php
@@ -13,7 +13,7 @@
 
                         <div class="heading-button">
                             <h2> My Upcoming Events </h2>
-                            <a href="#" class="btn btn--view-all"> View all</a>
+                            <a href="{{url('aloha-events')}}" class="btn btn--view-all"> View all</a>
                         </div>
                         
                         <div class="chapter-events-upcoming">
@@ -21,7 +21,7 @@
                             <div class="chapter-events-upcoming__box">
                                 <div class="row">
                                     <div class="col-md-3 chapter-events-upcoming__image">
-                                        <a href="#">
+                                        <a href="{{url('aloha-events')}}">
                                             <img src="{{ url('public/images/event-preview.jpg') }}" alt="event title" class="img-fluid">
                                         </a>
                                     </div>
@@ -33,7 +33,7 @@
                                              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco lab.
                                                  oris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
                                             </div>
-                                             <a href="#" class="btn btn--primary"> Read More Details <i class="fas fa-arrow-right"></i> </a>
+                                             <a href="{{url('aloha-events')}}" class="btn btn--primary"> Read More Details <i class="fas fa-arrow-right"></i> </a>
                                         </div>
                                     </div>
                                 </div>
