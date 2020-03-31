2a6a3d7902e17c5ee998568eba06c8091b58bc8a
dnr_william <frontend@dogandrooster.com>
2020-03-31 11:55:22 +0800

    added css update for responsve and slider

--- public/js/app.js
+++ public/js/app.js
@@ -47049,7 +47049,31 @@ $('.events-camp-slider').slick({
   autoplay: true,
   autoplaySpeed: 5000,
   cssEase: 'linear',
-  arrows: true
+  arrows: true,
+  responsive: [{
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: true
+    }
+  }, {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 2
+    }
+  }, {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    } // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+
+  }]
 }); // $('.global-featured-slider__top--slick').slick({
 //     lazyLoad: 'ondemand',
 //     slidesToShow: 1,
