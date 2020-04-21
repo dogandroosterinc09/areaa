a024e47369835a8d0c1840c57be37d110b72247c
dnr_william <frontend@dogandrooster.com>
2020-04-21 08:03:39 +0800

    replaced login on mainpage.

--- resources/views/front/auth/login.blade.php
+++ resources/views/front/auth/login.blade.php
@@ -19,12 +19,12 @@
 
     
         {{-- need to dynamic this sub  --}}
-        <section class="masking-background login-mask">
+        <section class="masking-background login-mask login-section" style="display:none !important;">
             <div class="masking-background__wrapper container-max">
                 <div class="masking-background__item">
                     <div class="container-max masking-background__container">
                         <div class="row">
-                            <div class="col-md-6 content-middle">
+                            <div class="col-md-6 content-middle login-section__left">
 
                                 <div class="masking-background__content">
                                       <!-- Log In Form -->
@@ -70,31 +70,31 @@
                                             </div>
                                         </div>
                                         <div class="form-group form-actions">
-                                            <div class="col-xs-6">
-                                                <label class="switch switch-primary">
-                                                    <input type="checkbox" id="remember" name="remember" {{ old('remember') ? 'checked' : ''}}>
-                                                    <span></span>
-                                                </label>
-                                                <small>Remember me</small>
-                                            </div>
-                                            <div class="col-xs-6 text-right">
-                                                <a href="{{url('dashboard-main')}}" class="btn btn--secondary"><i class="fa fa-arrow-right"></i>
-                                                    Log In
-                                                </a>
-                                                {{-- <button type="submit" class="btn btn--secondary"><i class="fa fa-arrow-right"></i>
-                                                    Log In
-                                                </button> --}}
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <label class="switch switch-primary">
+                                                        <input type="checkbox" id="remember" name="remember" {{ old('remember') ? 'checked' : ''}}>
+                                                        <span></span>
+                                                        <small>Remember me</small>
+                                                    </label>
+                                                    <div class="no-account">Don't have any account ? <a href="#"> Sign Up Register</a></div>
+                                                    <div class="lost-password">Remind <a href="{{ url('customer/password/email') }}"> Forgot password?</a></div>
+                                                </div>
+                                                <div class="col-md-12 text-right">
+                                                    <a href="{{url('dashboard-main')}}" class="btn btn--secondary"><i class="fa fa-arrow-right"></i>
+                                                        Log In
+                                                    </a>
+                                                    {{-- <button type="submit" class="btn btn--secondary"><i class="fa fa-arrow-right"></i>
+                                                        Log In
+                                                    </button> --}}
+                                                </div>
                                             </div>
                                         </div>
                                         <div class="form-group">
 
                                         </div>
                                     {{ Form::close() }}
-                                    <div class="text-center">
-                                        <a href="{{ url('customer/password/email') }}">
-                                            <small>Forgot password?</small>
-                                        </a>
-                                    </div>
+                                   
                                     <!-- END Log In Form -->
                                 </div>
 
@@ -108,6 +108,90 @@
                 <img src="{{ url('public/images/our-story-image.jpg') }}">
             </div>
         </section>
+
+
+
+               {{-- individual section  --}}
+               <section class="login-section login-section-main">
+                <div class="container-max">
+                    <div class="row">
+    
+    
+                        <div class="col-lg-6 login-section__left login-section__item">
+                          
+                            <div class="login-section__form">
+                                <form>
+                                    {{  Form::open([
+                                        'method' => 'POST',
+                                        'id' => 'form-login',
+                                        'route' => ['customer.login.post'],
+                                        'class' => 'form-horizontal'
+                                        ])
+                                    }}
+                                    <div class="row">
+                                        <div class="col-md-12 form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                            {{-- <label for="email">Email</label> --}}
+                                            <input id="email" type="text" class="form-control " name="email" placeholder="Username or Email Address *">
+                                            @if($errors->has('email'))
+                                                <span class="help-block animation-slideDown">{{ $errors->first('email') }}</span>
+                                            @endif
+                                        </div>
+                                        <div class="col-md-12 form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                            {{-- <label for="password">Subject</label> --}}
+                                            <input id="password" type="password" class="form-control " name="password" placeholder="Password*">
+                                            @if($errors->has('password'))
+                                                <span class="help-block animation-slideDown">{{ $errors->first('password') }}</span>
+                                            @endif
+                                        </div>
+                                        <div class="col-md-6 form-group{{ $errors->has('rember') ? ' has-error' : '' }}">
+                                          
+                                            <ul class="custom-checkbox">
+                                                <li class="custom-checkbox__item">
+                                                    <label class="custom-checkbox__item--holder">
+                                                        <input type="checkbox">
+                                                            Remember Me
+                                                        <span class="checkmark custom-checkbox__checkmark custom-checkbox__checkmark--boxstyle"></span>
+                                                    </label>
+                                                </li>
+                                            </ul>
+                                        
+                                            @if($errors->has('subject'))
+                                                <span class="help-block animation-slideDown">{{ $errors->first('subject') }}</span>
+                                            @endif
+                                        </div>
+                                        <div class="col-md-6 form-group text-right">
+                                            <button type="submit" class="btn btn--secondary">SUBMIT</button>
+                                            <a href="#" class="btn btn--forgot"> Forgot your password? </a>
+                                        </div>
+                                    </div>
+                                    {{ Form::close() }}
+                                </form>
+                            </div>
+    
+                        </div>
+    
+    
+                        <div class="col-lg-6 login-section__right login-section__item">
+    
+                            <div class="login-section__message image-background">
+                                <img src="{{ url('public/images/our-story-image.jpg') }}" alt="event title" class="img-fluid">
+                                
+                                <div class="login-section__content">
+                                    <h4>Not a member yet? Join us today!</h4>
+                                    <p>Lorem ipsum dolor sit amet, quam sollicitudin sagittis fringilla lacus enim, leo elit non nec varius sodales. Amet faucibus, id tempor quisque pharetra leo. Curae integer. Diam duis integer vel ut. </p>
+                                    <div class="btn-group text-right">
+                                    <a href="{{url('membership-registration')}}" class="btn btn btn--secondary">Join AREAA!</a>
+                                   </div>
+                                </div>
+    
+                            </div>
+                           
+                        </div>
+                           
+                    </div>
+                </div> {{-- end of default-content--row --}}
+            </section> {{-- end of default-content --}}
+    
 
 
 
