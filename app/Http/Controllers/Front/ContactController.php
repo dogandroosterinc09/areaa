aad6ff60e3d8e688e0efbc2cdeb75bb1152cfa84
dnr_mark <programmer20@dogandrooster.com>
2020-05-30 12:03:36 +0800

    temporary skip email in contact us

--- app/Http/Controllers/Front/ContactController.php
+++ app/Http/Controllers/Front/ContactController.php
@@ -56,29 +56,30 @@ class ContactController extends Controller
 
         $contact = $this->contact->create($request->all());
 
-        if (!in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
-            $email_data = [
-                'view' => 'email.contact',
-                'type' => 'contact',
-                'user' => [
-                    'name' => $contact->name,
-                    'email' => $contact->email,
-                ],
-                'user_data' => $contact,
-                'subject' => 'Thank you for contacting us.',
-                'attachments' => [],
-                'is_admin' => false,
-            ];
+        //Skip Email
+        // if (!in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'])) {
+        //     $email_data = [
+        //         'view' => 'email.contact',
+        //         'type' => 'contact',
+        //         'user' => [
+        //             'name' => $contact->name,
+        //             'email' => $contact->email,
+        //         ],
+        //         'user_data' => $contact,
+        //         'subject' => 'Thank you for contacting us.',
+        //         'attachments' => [],
+        //         'is_admin' => false,
+        //     ];
 
-            // send email to customer
-            $this->contactRepository->sendEmail($email_data);
+        //     // send email to customer
+        //     $this->contactRepository->sendEmail($email_data);
 
-            // send email to admin
-            $email_data['is_admin'] = true;
-            $email_data['view'] = 'email.contact_admin';
-            $email_data['subject'] = 'New Contact Us Form';
-            $this->contactRepository->sendEmail($email_data);
-        }
+        //     // send email to admin
+        //     $email_data['is_admin'] = true;
+        //     $email_data['view'] = 'email.contact_admin';
+        //     $email_data['subject'] = 'New Contact Us Form';
+        //     $this->contactRepository->sendEmail($email_data);
+        // }
 
         return redirect()->back()->with('flash_message', [
             'title' => '',
