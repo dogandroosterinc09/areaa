2687311c3e2f21b20c67fc2a14d957292b2f9899
dnr_william <frontend@dogandrooster.com>
2020-06-19 11:58:18 +0800

    pleacegolder message

--- resources/views/front/pages/custom-page/chapter-contactus.blade.php
+++ resources/views/front/pages/custom-page/chapter-contactus.blade.php
@@ -80,7 +80,8 @@
                                 </div>
                                 <div class="col-md-12 form-group {{ $errors->has('message') ? ' has-error' : '' }}">
                                     <label for="message">Message</label>
-                                    <textarea id="message" name="message" class="form-control" placeholder="Message"> </textarea>
+                                    {{-- <textarea id="message" name="message" class="form-control" placeholder="Message"> </textarea> --}}
+                                    <textarea id="message" name="message" class="form-control" placeholder="Message*" spellcheck="false"></textarea>
                                     @if($errors->has('message'))
                                         <span class="help-block animation-slideDown">{{ $errors->first('message') }}</span>
                                     @endif
